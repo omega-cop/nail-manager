6dcecd7051fcdc8aa565f4eade6619d5862811ff
omega-cop <omegagreen4@gmail.com>
2025-11-27 12:34:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    // Load env file based on `mode` in the current working directory.
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), '');
+    
     return {
       server: {
         port: 3000,
@@ -24,6 +27,7 @@ export default defineConfig(({ mode }) => {
             display: 'standalone',
             orientation: 'portrait',
             start_url: '/',
+            scope: '/',
             icons: [
               {
                 src: 'pwa-192x192.png',
@@ -42,6 +46,11 @@ export default defineConfig(({ mode }) => {
                 purpose: 'any maskable'
               }
             ]
+          },
+          workbox: {
+             globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+             cleanupOutdatedCaches: true,
+             maximumFileSizeToCacheInBytes: 5000000
           }
         })
       ],
@@ -51,7 +60,7 @@ export default defineConfig(({ mode }) => {
       },
       resolve: {
         alias: {
-          '@': path.resolve('.'),
+          '@': path.resolve(process.cwd()),
         }
       }
     };
